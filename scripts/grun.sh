a444773eeb4cdc2be57d531fc40ea0e50df41ff9
Hamid EL MAAZOUZ <hamid.elmaazouz@gmail.com>
2022-08-12 16:26:36 +0100

    grun

--- scripts/grun.sh
+++ scripts/grun.sh
@@ -1,10 +1,21 @@
-# mkdir -p /tmp/antlr/grun
-# export OUTPUT_DIR=/tmp/antlr/grun
-export OUTPUT_DIR=/home/hamidelmaazouz/repositories/antlr/antlr4/runtime/Java/src/org/antlr/v4/runtime/pal/evaluation
 export ANTLR_REPO=/home/hamidelmaazouz/repositories/antlr/antlr4
-export CLASSPATH=$OUTPUT_DIR:$ANTLR_REPO/tool/target/antlr4-4.10.2-SNAPSHOT-complete.jar
-export WORKING_DIR=$(pwd)
+export PWD=$(pwd)
 
-java org.antlr.v4.Tool -no-listener -visitor -o $OUTPUT_DIR $WORKING_DIR/example/java/Java8.g4
-# javac -d $OUTPUT_DIR $OUTPUT_DIR/*.java
-# java org.antlr.v4.gui.TestRig Java8 compilationUnit -cfg $WORKING_DIR/example/java/Fibonacci.java
+### Build ANTLR from source - build once and comment these lines to save time
+#cd $ANTLR_REPO/scripts
+#source install.sh
+#cd $PWD
+#exit
+
+### Use the previously built ANTLR to generate target language artefacts
+export RUST_GRAMMAR_DIR=$PWD/rust
+export ANTLR_OUTPUT_DIR=$RUST_GRAMMAR_DIR/Java
+export JAVA_OUTPUT_DIR=$RUST_GRAMMAR_DIR/target/classes
+export CLASSPATH=$ANTLR_REPO/tool/target/antlr4-4.10.2-SNAPSHOT-complete.jar
+
+java org.antlr.v4.Tool -listener -visitor -cfgbuilder -o $ANTLR_OUTPUT_DIR \
+     $RUST_GRAMMAR_DIR/RustLexer.g4 $RUST_GRAMMAR_DIR/RustParser.g4
+javac -d $JAVA_OUTPUT_DIR $ANTLR_OUTPUT_DIR/*.java
+
+export CLASSPATH=$CLASSPATH:$JAVA_OUTPUT_DIR
+java org.antlr.v4.gui.TestRig Rust crate -cfg $RUST_GRAMMAR_DIR/examples/hello.rs
